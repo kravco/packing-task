7e22387beda17ab4390d7f8b486a31e7dd441d1f
Matej Kravjar <115938+kravco@users.noreply.github.com>
2024-10-03 10:12:43 +0200

    chore: add phpstan, testing script update

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 - `composer install && vendor/bin/doctrine orm:schema-tool:create && vendor/bin/doctrine dbal:run-sql "$(cat data/packaging-data.sql)"`
 
 ### run
-- `php run.php "$(cat sample.json)"`
+- ` export CREDENTIALS_USERNAME=your_3dbinpacking_username`
+- ` export CREDENTIALS_API_KEY=your_3dbinpacking_api_key`
+- `CREDENTIALS_USERNAME=$CREDENTIALS_USERNAME CREDENTIALS_API_KEY=$CREDENTIALS_API_KEY php run.php "$(cat sample.json)"`
 
 ### adminer
 - Open `http://localhost:8080/?server=mysql&username=root&db=packing`
--- run.php
+++ run.php
@@ -1,17 +1,46 @@
 <?php
 
 use App\Application;
+use App\CredentialsInterface;
 use Doctrine\ORM\EntityManager;
 use GuzzleHttp\Psr7\Message;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\Psr7\Uri;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\Cache\Psr16Cache;
 
 /** @var EntityManager $entityManager */
 $entityManager = require __DIR__ . '/src/bootstrap.php';
+$requestFactory = new Http\Factory\Guzzle\RequestFactory;
+$responseFactory = new Http\Factory\Guzzle\ResponseFactory;
+$streamFactory = new Http\Factory\Guzzle\StreamFactory;
+$httpClient = new \GuzzleHttp\Client;
+$simpleCache = new Psr16Cache(new ArrayAdapter);
+$credentials = new class implements CredentialsInterface {
+    public function getUsername(): string
+    {
+        $value = getenv('CREDENTIALS_USERNAME');
+        return $value === false ? null : $value;
+    }
+
+    public function getApiKey(): SensitiveParameterValue
+    {
+        $value = getenv('CREDENTIALS_API_KEY');
+        return new SensitiveParameterValue($value === false ? null : $value);
+    }
+};
 
 $request = new Request('POST', new Uri('http://localhost/pack'), ['Content-Type' => 'application/json'], $argv[1]);
 
-$application = new Application($entityManager);
+$application = new Application(
+    $entityManager,
+    $requestFactory,
+    $responseFactory,
+    $streamFactory,
+    $httpClient,
+    $simpleCache,
+    $credentials,
+);
 $response = $application->run($request);
 
 echo "<<< In:\n" . Message::toString($request) . "\n\n";
